1f81d7351122d63d5da4a839681cdbcc8cc6101d
sasicode7010 <sasicode7010@gmail.com>
2025-08-14 11:15:04 +0530

    First commit

---
+++ comment.js
@@ -0,0 +1,5 @@
+console.log("this is the basic comment program !!!");
+
+console.log("this is single line comment //");
+
+console.log("use the /* */ for the multi line comment !!!")
---
+++ first.js
@@ -0,0 +1,5 @@
+console.log("hi every one");
+
+let a =10;
+
+console.log(a);
